a6cfedc6ad8fa9cabd243031ffe902ded4e54700
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-16 22:19:09 +0900

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const Header = (props) => (
-            <nav>
-                <span className='title_logo'><img src="../images/logo.png" alt="" />YouTube</span>
-                <form action=""className='search_form'>
-                    <input type="text" placeholder='Search...' className='search_input'/>
-                    <button className='search_btn'><img src="../images/search.png" alt="" /></button>
-                </form>
-            </nav>
-    );
+function Header({search}){
+    const inputRef = useRef();
+    const handleSearch = ()=>{
+        const value = inputRef.current.value;
+        search(value);
+    };
+    const onClick = ()=>{
+        handleSearch();
+    };
+    const onKeyPress = (event)=>{
+        if(event.key === 'Enter'){
+            handleSearch();
+        }
+        
+    };
+
+    return(
+        <header>
+            <span className='title_logo'>
+                <img src="../images/logo.png" alt="" />YouTube
+            </span>
+                <div className='search_form'>
+                <input 
+                    ref={inputRef}
+                    type="text" 
+                    placeholder='Search...' 
+                    className='search_input'
+                    onKeyPress={onKeyPress}
+                    />
+                <button 
+                className='search_btn'
+                onClick={onClick}
+                >
+                <img src="../images/search.png" alt="" /></button>
+                </div>
+        </header>
+            );
+        };
 
 export default Header;
