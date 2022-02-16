8076ac03f8cf365dec4bc2e2bcf229ed7217e537
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-16 19:44:55 +0900

    api

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import './app.css';
-import Header from './components/header';
+import VideoList from './components/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBBc1cdK2nDD3kQG6hCuc5sdbXcosBVB7s",
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
   return (
     <>
-    <Header/>
+    <VideoList videos={videos} />
     </>
   );
 }
