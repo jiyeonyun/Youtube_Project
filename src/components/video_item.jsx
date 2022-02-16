addafbbae182dd6b6bc27e70b9c17fb0012e517e
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-16 22:19:18 +0900

    video items

--- src/components/video_item.jsx
+++ src/components/video_item.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = (props) => (
+            <li className='itme_list'>
+                <img src={props.video.snippet.thumbnails.medium.url} alt="" width="320px" height="180px" />
+                <div className='item_desc'>
+                <h1 className='item_title'>
+                    {props.video.snippet.title.length > 28 ? props.video.snippet.title.substr(0,26)+'...' : props.video.snippet.title}
+                    </h1>
+                <span className='item_date'>{props.video.snippet.publishedAt}</span>
+                <span className='item_channelName'>{props.video.snippet.channelTitle}</span>
+                </div>
+            </li>
+    );
 
 export default VideoItem;
+
