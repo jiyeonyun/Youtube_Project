593a6f23cdea61c231c2acbef26450ba317c0eb1
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-17 20:14:12 +0900

    props

--- src/components/video_item.jsx
+++ src/components/video_item.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const VideoItem = (props) => (
-            <li className='itme_list'>
-                <img src={props.video.snippet.thumbnails.medium.url} alt="" width="320px" height="180px" />
-                <div className='item_desc'>
-                <h1 className='item_title'>
-                    {props.video.snippet.title.length > 28 ? props.video.snippet.title.substr(0,26)+'...' : props.video.snippet.title}
-                    </h1>
-                <span className='item_date'>{props.video.snippet.publishedAt}</span>
-                <span className='item_channelName'>{props.video.snippet.channelTitle}</span>
-                </div>
-            </li>
+function VideoItem(props){
+    const handleClick = (event) =>{
+        props.setSelectVideo(true);
+        let id = (event.target.id);
+        console.log(id)
+        props.detail(id);
+    };
+    return(
+        <li className='itme_list' onClick={handleClick} id={props.video.id}>
+        <img src={props.video.snippet.thumbnails.medium.url} id={props.video.id} alt="" width="320px" height="180px" />
+        <div className='item_desc' id={props.video.id}>
+        <h1 className='item_title' id={props.video.id}>
+            {props.video.snippet.title.length > 28 ? props.video.snippet.title.substr(0,26)+'...' : props.video.snippet.title}
+            </h1>
+        <span className='item_date' id={props.video.id}>{props.video.snippet.publishedAt}</span>
+        <span className='item_channelName' id={props.video.id}>{props.video.snippet.channelTitle}</span>
+        </div>
+    </li>
     );
+    }
+            
 
 export default VideoItem;
 
