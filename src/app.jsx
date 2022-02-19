b00fe8a2d7e84a6b7bf76745edf0da267036ad3b
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-19 22:12:35 +0900

    delete components

--- src/app.jsx
+++ src/app.jsx
@@ -1,50 +1,9 @@
 import { useEffect, useState } from 'react';
 import './app.css';
-import VideoList from './components/video_list';
-import Header from './components/header'
-import Detail from './components/detail';
 
-function App() {
-  const [videos, setVideos] = useState([]);
-  const [selectVideo,setSelectVideo] = useState(false);
-  const [id,setId] = useState('');
-  const detail = (id)=>{
-    setId(id);
-  }
-  const search = query =>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyBBc1cdK2nDD3kQG6hCuc5sdbXcosBVB7s`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  };
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=AIzaSyBBc1cdK2nDD3kQG6hCuc5sdbXcosBVB7s",
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
-
-  return (
-    <>
-    <Header search={search} />
-    {selectVideo && <Detail videos={videos} id={id}/>}
-    <VideoList videos={videos} detail={detail} selectVideo={selectVideo} setSelectVideo={setSelectVideo}/>
-    </>
-  );
+function App(){
+  const[videos, setVideos] = useState([]);
+  return <h1>hi</h1>;
 }
 
 export default App;
