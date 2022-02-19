7f60ca079d1147f6c64b5268257805fd66cb2eda
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-19 22:26:43 +0900

    components update

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,25 @@
 import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App(){
   const[videos, setVideos] = useState([]);
-  return <h1>hi</h1>;
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBBc1cdK2nDD3kQG6hCuc5sdbXcosBVB7s", 
+      requestOptions
+      )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
