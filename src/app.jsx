38985d46f9577f5b84226baab55336fa020e6313
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-19 23:29:27 +0900

    search api

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,20 @@ import VideoList from './components/video_list/video_list';
 
 function App(){
   const[videos, setVideos] = useState([]);
-
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBBc1cdK2nDD3kQG6hCuc5sdbXcosBVB7s`, requestOptions)
+      .then(response => response.json())
+      .then(result => 
+          result.items.map(item =>({...item, id :item.id.videoId}))
+          )
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }; 
   useEffect(()=>{
     const requestOptions = {
       method: 'GET',
@@ -22,7 +35,7 @@ function App(){
   },[]);
   return (
     <div className={styles.app}>
-    <SearchHeader />
+    <SearchHeader onSearch={search}/>
     <VideoList videos={videos} />
     </div>
   );
